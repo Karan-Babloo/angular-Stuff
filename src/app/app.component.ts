81789ce8a1d64370cbe84656cad01ea516ea1ce7
Karan Babloo <karan.m@gmx.com>
2020-03-02 11:50:28 -0500

    Update Regular

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -38,4 +38,49 @@ export class AppComponent implements AfterViewInit{
         const data: Blob = new Blob([buffer], { type: 'application/vnd.openxmlformats-officedocument.spreadsheetml.sheet;charset=UTF-8' });
         FileSaver.saveAs(data, fileName + '.xlsx');
     }*/
+  
+  /*
+  public async apiHttpPostAsPromiseWithAwait(url, requestParams): Promise<ApiResponse> {
+        const headers = new Headers();
+        headers.append('Content-Type', 'application/json');
+        headers.append('applicationName', 'name');
+        const body = JSON.stringify(requestParams);
+        //stackoverflow.com/questions/49444816/angular-5-synchronous-http-call
+        return await this.apiHttp
+            .post(url, body, { headers: headers })
+            .toPromise();
+    }
+    
+    //For Upload
+    
+    
+    public apiHttpPostFormData<T>(url, requestParams, mapResponse: boolean = true): Observable<{} | Observable<T[]>> {
+        const headers = new Headers();
+        //Do not add the 'content-type:application/json' to headers when form-data/multipart is been sent.
+        headers.append('applicationName', 'name');
+        return this.apiHttp
+            .post(url, requestParams, { headers: headers })
+            .map((apiResponse: ApiResponse) => {
+                return mapResponse ? (apiResponse.data as T[]) : apiResponse;
+            })
+            .catch((apiResponse: any) => this.handleError(apiResponse));
+    }
+    
+    // Regular API
+    
+     public apiHttpPostPdf<T>(url, requestParams): Observable<{} | Observable<T[]>> {
+        const headers = new Headers();
+        //application/pdf format is not supported by our services so use application/json and create blob objects for generating pdfs
+        headers.append('Content-Type', 'application/json');
+        headers.append('applicationName', 'name');
+        const responseType = ResponseContentType.ArrayBuffer;
+        const body = JSON.stringify(requestParams);
+        return this.apiHttp
+            .postReport(url, body, { headers: headers, responseType: responseType })
+            .map((apiResponse) => {
+                return apiResponse;
+            })
+            .catch((apiResponse: any) => this.handleError(apiResponse));
+    }
+  */
 }
